39a1e785604204482caa7561971e7d34d1e14915
Parmesan123 <safonenko44@gmail.com>
2026-01-25 00:19:28 +0100

    renamed HasAncestor method in GodotExtensionUtils to HasParent

--- scripts/utils/GodotExtensionUtils.cs
+++ scripts/utils/GodotExtensionUtils.cs
@@ -18,10 +18,10 @@ public static class GodotExtensionUtils
         return null;
     }
 
-    public static bool HasAncestor<TScript>(this Node targetNode) where TScript : Node => targetNode.FindParent<TScript>() != null;
+    public static bool HasParent<TScript>(this Node targetNode) where TScript : Node => targetNode.FindParent<TScript>() != null;
 
     public static TScript FindChild<TScript>(this Node targetNode) where TScript : Node
     {
-        targetNode.Find
+        throw new NotImplementedException();
     }
 }
